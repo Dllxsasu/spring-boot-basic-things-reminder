4799fb1f1e1776acb3416165ddec152ab34d087c
jeremias <43934979+Dllxsasu@users.noreply.github.com>
2023-05-02 01:24:58 -0500

    asd

---
+++ render-PoC-cloud/src/main/java/com/jeremias/dev/renderpoccloud/RenderPoCCloudApplication.java
@@ -0,0 +1,13 @@
+package com.jeremias.dev.renderpoccloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RenderPoCCloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RenderPoCCloudApplication.class, args);
+    }
+
+}
---
+++ render-PoC-cloud/src/test/java/com/jeremias/dev/renderpoccloud/RenderPoCCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeremias.dev.renderpoccloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RenderPoCCloudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
